fd56c31c990d928ed8c14fb8faaa5f51cc72cce9
Ann McCartney <ann@acmn.com.au>
2016-06-23 17:57:13 +1000

    Add in Youtube Video

--- partials/about-home.php
+++ partials/about-home.php
@@ -7,15 +7,11 @@
 	<div class="row">
 		<div class="col-xs-12 about-content">
 			<div class="row">
-				<!-- <div class="col-xs-12 col-md-6 left-column"> -->
-<!-- 					<p>The iconic Marina Prior will join Australia's #1 tenor Mark Vincent in a magical evening performing unforgettable aria's from Opera, musical theatre and contemporary song.</p>
-					<p>A leading lady of musical theatre, Marina Prior has performed over twenty leading roles in her career, starring in such musicals as The Phantom of the Opera, Cats, Les Miserables, Guys and Dolls and Mary Poppins. She is currently starring as the Baroness Schraeder in the Australian tour of The Sound of Music.</p>
- -->				<!-- </div> --><!-- /.col-xs-12 .col-md-6 -->
-				<!-- <div class="col-xs-12 col-md-6 right-column"> -->
-<!-- 					<p>After winning Australia's Got Talent at the age of 15, Mark Vincent, now 22, has 6 albums under his belt. He recently starred on stage in the highly acclaimed musical Dirty Dancing, and is already making a name for himself in Australian Opera and Musical Theatre.</p>
-					<p>Singing all the classics from their latest album, from "Amigos Para Seimpre" to "All I Ask Of You" and "Hate You Then I Love You", this magical evening promises to be a night of fine entertainment with two of Australia's most heavenly voices. 
- -->				<!-- </div> --><!-- /.col-xs-12 .col-md-6 -->
  				<div class="col-xs-12">
+ 					<div class="embed-responsive embed-responsive-16by9">
+					  <iframe class="pull-right"  src="https://www.youtube.com/embed/h7swK12J4LE?rel=0" allowfullscreen></iframe>
+					</div>
+ 					<p></p>
  					<p class="lead">For one night only, the iconic Marina Prior will join Australia's No.1 tenor Mark Vincent in a magical performance of soaring opera arias, musical theatre showstoppers and well-loved popular songs.</p>
  					<p>One of Australia's greatest leading ladies, Marina Prior has performed over twenty leading roles in her musical theatre career, starring in such shows as <strong>The Phantom of the Opera</strong>, <strong>Cats</strong>, <strong>Les Misérables</strong>, <strong>Guys and Dolls</strong> and <strong>Mary Poppins</strong>. She is currently starring as Baroness Schraeder in the Australian tour of <strong>The Sound of Music</strong>.</p>
  					<p>Mark Vincent is a prodigious singing talent, with legions of fans nationwide after winning Australia’s Got Talent at the age of 15. Now 22, has six albums under his belt, with over 500,000 plus sales in Australia alone, making him a multi platinum artist. He recently starred on stage in the highly acclaimed musical <strong>Dirty Dancing</strong>, and is making a name for himself in Australian opera and musical theatre.</p[>
